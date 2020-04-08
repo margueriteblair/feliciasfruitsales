ae4ef7ac9c00dde36389c2c0ee70cba1bf3fbcb0
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-07 21:23:18 -0400

    added sales calculation, removed fruitinventory console

--- fruitsales.js
+++ fruitsales.js
@@ -1,6 +1,7 @@
 let fruitInventory = ["Apples", "Peaches", "Pumpkins", "Endives"];
 let fruitPrices = [1, 1.5, 7, 12];
+let fruitSales = [30, 17, 20, 10];
 
 for (let i = 0; i < fruitInventory.length; i++) {
-    console.log(`${fruitInventory[i]}`);
+    console.log(fruitPrices[i]*fruitSales[i]);
 }
